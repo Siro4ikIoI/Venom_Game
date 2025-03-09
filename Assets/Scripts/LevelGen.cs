d7f1e32dfa4ae84b15a3b1d41c7224f0d6be276f
Siro4ikIoI <galstansuren69@gmail.com>
2025-03-09 16:54:52 +0300

    Procedural map generation _Alpha🌳🌳🌳

--- Assets/Scripts/LevelGen.cs
+++ Assets/Scripts/LevelGen.cs
@@ -4,15 +4,76 @@ using UnityEngine;
 
 public class LevelGen : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] mapRoom;
+    public int maxRooms = 10;
+    private int currentRoomCount = 1;
+
     void Start()
     {
-        
+        GameObject mainRoom = Instantiate(mapRoom[0]);
+        mainRoom.transform.position = new Vector3(0, 0, 0);
+        GenRoom(mainRoom.transform, 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GenRoom(Transform parentRoom, int roomIndex)
     {
-        
+        if (currentRoomCount >= maxRooms)
+            return;
+
+        List<Transform> conectors = GetConnectors(parentRoom);
+
+        foreach (Transform connector in conectors)
+        {
+            if (connector != null)
+            {
+                GameObject room = Instantiate(mapRoom[roomIndex]);
+                currentRoomCount++;
+
+                connector.SetParent(null);
+                Vector3 newRoomPosition = GetRoomPosition(connector, room);
+
+                room.transform.position = newRoomPosition;
+
+                GenRoom(room.transform, roomIndex);
+            }
+        }
+    }
+
+    private List<Transform> GetConnectors(Transform room)
+    {
+        List<Transform> connectors = new List<Transform>();
+        foreach (Transform child in room)
+        {
+            if (child.name == "Conecter") connectors.Add(child);
+        }
+        return connectors;
+    }
+
+    private Vector3 GetRoomPosition(Transform connector, GameObject room)
+    {
+        Vector3 position = Vector3.zero;
+
+        if (connector.position.x > 0)
+        {
+            room.transform.Rotate(0, -90, 0);
+            position = new Vector3(connector.position.x * -2, 0, connector.position.z);
+        }
+        else if (connector.position.x < 0)
+        {
+            room.transform.Rotate(0, 90, 0);
+            position = new Vector3(connector.position.x * -2, 0, -connector.position.z);
+        }
+        else if (connector.position.z > 0)
+        {
+            room.transform.Rotate(0, 0, 0);
+            position = new Vector3(0, 0, connector.position.z * 2);
+        }
+        else if (connector.position.z < 0)
+        {
+            room.transform.Rotate(0, 180, 0);
+            position = new Vector3(0, 0, connector.position.z * 2);
+        }
+
+        return position;
     }
 }
