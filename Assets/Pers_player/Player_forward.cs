faf8d6fdd7603a49bffd295ce4d1b18b23960447
Chipp <Andreyjasam@gmail.com>
2025-03-10 23:46:24 +0300

    ypravlenie

---
+++ Assets/Pers_player/Player_forward.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_forward : MonoBehaviour
+{
+    public TohScreen TohScreen;
+    public Joystick Joystick_p;
+    public float Speed_Player;
+    public bool mobil;
+    private float xhR = 0f;
+    private float yhR = 0f;
+
+    private Camera Main_Camera;
+    void Start()
+    {
+        Main_Camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float mouseX = 0;
+        float mouseY = 0;
+
+        if (TohScreen.pressed)
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.fingerId == TohScreen.finger)
+                {
+                    if (touch.phase == TouchPhase.Moved)
+                    {
+                        mouseX += touch.deltaPosition.x * Time.deltaTime * TohScreen.fingerSpeed;
+                        mouseY += touch.deltaPosition.y * Time.deltaTime * TohScreen.fingerSpeed;
+                        xhR += mouseX;
+                        yhR += mouseY;
+                        Main_Camera.transform.localRotation = Quaternion.Euler(yhR, xhR, 0);
+                    }
+                    if (touch.phase == TouchPhase.Stationary)
+                    {
+                        mouseX = 0;
+                    }
+                }
+            }
+        }
+
+        float hor_input = 0f;
+        float ver_input = 0f;
+
+        hor_input = Joystick_p.Horizontal;
+        ver_input = Joystick_p.Vertical;
+        Vector3 moveVektor = (new Vector3(Main_Camera.transform.forward.x, 0f, Main_Camera.transform.forward.z) * ver_input + Main_Camera.transform.right * hor_input) * Time.deltaTime * Speed_Player;
+        transform.Translate(moveVektor, Space.World);
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0 , Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
