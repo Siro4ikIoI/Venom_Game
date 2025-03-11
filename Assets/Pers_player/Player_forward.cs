cf5c3e3102f7869c1c8cdcd9e0ecc8a5bd99e5a8
Chipp <Andreyjasam@gmail.com>
2025-03-11 22:42:44 +0300

    Ai_nav

--- Assets/Pers_player/Player_forward.cs
+++ Assets/Pers_player/Player_forward.cs
@@ -20,39 +20,54 @@ public class Player_forward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        float mouseX = 0;
-        float mouseY = 0;
-
-        if (TohScreen.pressed)
+        if (mobil)
         {
-            foreach (Touch touch in Input.touches)
+            float mouseX = 0;
+            float mouseY = 0;
+
+            if (TohScreen.pressed)
             {
-                if (touch.fingerId == TohScreen.finger)
+                foreach (Touch touch in Input.touches)
                 {
-                    if (touch.phase == TouchPhase.Moved)
+                    if (touch.fingerId == TohScreen.finger)
                     {
-                        mouseX += touch.deltaPosition.x * Time.deltaTime * TohScreen.fingerSpeed;
-                        mouseY += touch.deltaPosition.y * Time.deltaTime * TohScreen.fingerSpeed;
-                        xhR += mouseX;
-                        yhR += mouseY;
-                        Main_Camera.transform.localRotation = Quaternion.Euler(yhR, xhR, 0);
-                    }
-                    if (touch.phase == TouchPhase.Stationary)
-                    {
-                        mouseX = 0;
+                        if (touch.phase == TouchPhase.Moved)
+                        {
+                            mouseX += touch.deltaPosition.x * Time.deltaTime * TohScreen.fingerSpeed;
+                            mouseY += touch.deltaPosition.y * Time.deltaTime * TohScreen.fingerSpeed;
+                            xhR += mouseX;
+                            yhR += mouseY;
+                            Main_Camera.transform.localRotation = Quaternion.Euler(-yhR, xhR, 0);
+                        }
+                        if (touch.phase == TouchPhase.Stationary)
+                        {
+                            mouseX = 0;
+                        }
                     }
                 }
             }
+
+            float hor_input = 0f;
+            float ver_input = 0f;
+
+            hor_input = Joystick_p.Horizontal;
+            ver_input = Joystick_p.Vertical;
+            Vector3 moveVektor = (new Vector3(Main_Camera.transform.forward.x, 0f, Main_Camera.transform.forward.z) * ver_input + Main_Camera.transform.right * hor_input) * Time.deltaTime * Speed_Player;
+            transform.Translate(moveVektor, Space.World);
+        }
+        else
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            xhR += mouseX;
+            yhR += mouseY;
+
+            Main_Camera.transform.localRotation = Quaternion.Euler(-yhR, xhR, 0);
+            Vector3 moveVektor = (new Vector3(Main_Camera.transform.forward.x, 0f, Main_Camera.transform.forward.z) * Input.GetAxis("Vertical") + Main_Camera.transform.right * Input.GetAxis("Horizontal")) * Time.deltaTime * Speed_Player;
+            transform.Translate(moveVektor, Space.World);
         }
 
-        float hor_input = 0f;
-        float ver_input = 0f;
-
-        hor_input = Joystick_p.Horizontal;
-        ver_input = Joystick_p.Vertical;
-        Vector3 moveVektor = (new Vector3(Main_Camera.transform.forward.x, 0f, Main_Camera.transform.forward.z) * ver_input + Main_Camera.transform.right * hor_input) * Time.deltaTime * Speed_Player;
-        transform.Translate(moveVektor, Space.World);
-        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0 , Input.GetAxis("Vertical") * Time.deltaTime);
+        
     }
 }
