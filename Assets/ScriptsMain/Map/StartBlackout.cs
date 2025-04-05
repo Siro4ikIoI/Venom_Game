f599a8a1a321721846c626b05328bc22c2435536
Siro4ikIoI <galstansuren69@gmail.com>
2025-04-05 17:27:44 +0300

    Update: optimization and localization

--- Assets/ScriptsMain/Map/StartBlackout.cs
+++ Assets/ScriptsMain/Map/StartBlackout.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using YG;
 
 public class StartBlackout : MonoBehaviour
 {
     [TextArea(1, 10)]
     public string[] text;
     [TextArea(1, 10)]
+    public string[] textEn;
+    [TextArea(1, 10)]
+    public string[] textTr;
+
+    [TextArea(1, 10)]
     public string task;
+    [TextArea(1, 10)]
+    public string taskEn;
+    [TextArea(1, 10)]
+    public string taskTr;
+
     private Coroutine cor;
     public GameObject[] dependentTriggers;
     public string tagObj;
@@ -26,7 +37,10 @@ public class StartBlackout : MonoBehaviour
         {
             if ((Inventory_list.instante.list_inventory[Inventory_list.instante.indx_now_obj].gameObject.tag == "ToolBOX") && !Messages.instance1._Dialogs)
             {
-                Messages.instance1.StartDialog(text, task, tagObj, dependentTriggers);
+                if (YandexGame.lang == "ru") Messages.instance1.StartDialog(text, task, tagObj, dependentTriggers);
+                else if (YandexGame.lang == "tr") Messages.instance1.StartDialog(textTr, taskTr, tagObj, dependentTriggers);
+                else Messages.instance1.StartDialog(textEn, taskEn, tagObj, dependentTriggers);
+
                 cor = StartCoroutine(StartCountdown());
             }
         }
--- Assets/ScriptsMain/Map/TrigerDialogs.cs
+++ Assets/ScriptsMain/Map/TrigerDialogs.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using YG;
 
 public class TrigerDialogs : MonoBehaviour
 {
     [TextArea(1, 10)]
     public string[] text;
     [TextArea(1, 10)]
+    public string[] textEn;
+    [TextArea(1, 10)]
+    public string[] textTr;
+
+    [TextArea(1, 10)]
     public string task;
+    [TextArea(1, 10)]
+    public string taskEn;
+    [TextArea(1, 10)]
+    public string taskTr;
+
     public GameObject[] dependentTriggers;
     public string tagObj;
 
@@ -19,7 +30,9 @@ public class TrigerDialogs : MonoBehaviour
 
         if (other.gameObject.tag == "Player" && !Messages.instance1._Dialogs)
         {
-            Messages.instance1.StartDialog(text, task, tagObj, dependentTriggers);
+            if (YandexGame.lang == "ru") Messages.instance1.StartDialog(text, task, tagObj, dependentTriggers);
+            else if (YandexGame.lang == "tr") Messages.instance1.StartDialog(textTr, taskTr, tagObj, dependentTriggers);
+            else Messages.instance1.StartDialog(textEn, taskEn, tagObj, dependentTriggers);
 
             if (dependentTriggers != null)
             {
